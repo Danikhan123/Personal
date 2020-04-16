2d7f8a3350f0b7f433ab03cc0489dbe71fe7dee6
danikhan123 <danikhan5710@gmail.com>
2020-04-16 16:15:07 +0500

    Initial commit

---
+++ src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_api);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+        LatLng islamabad=new LatLng(33.567793, 73.037008);
+        map.addMarker(new MarkerOptions().position(islamabad).title("ISLAMABAD"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(islamabad));
+
+    }
+}
